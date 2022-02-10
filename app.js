800e450134309508630bb608087f71f743b85b79
Preet Sojitra <preet.dev373@gmail.com>
2022-02-10 21:49:47 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const value = document.querySelector('#value');
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
+    // console.log(btn);
     btn.addEventListener("click", function(e){
         const styles = e.currentTarget.classList;
+        // console.log(styles);
         if(styles.contains("decrease")){
             count--;
         }
